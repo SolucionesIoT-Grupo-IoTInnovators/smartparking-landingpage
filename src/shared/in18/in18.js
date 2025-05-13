edd5286726d5ee58f8e09a310d674fba7e948beb
U202114545 <u202114545@upc.edu.pe>
2025-05-12 22:04:03 -0500

    feat: add i18n support
    Co-Authored-By: CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
    Co-Authored-By: FernandoDiazNeo <205968129+FernandoDiazNeo@users.noreply.github.com>
    Co-Authored-By: Johan Príncipe Godoy <73038573+JP19-03@users.noreply.github.com>
    Co-Authored-By: Josty Tafur <89022172+JostyTafur@users.noreply.github.com>
    Co-Authored-By: anonimat0 <103579781+anonimat0@users.noreply.github.com>
    Co-Authored-By: Ernesto Zarate <112828936+ern23zc@users.noreply.github.com>

---
+++ src/shared/in18/in18.js
@@ -0,0 +1,443 @@
+const in18 = {
+    ES: {
+        name: "Smart Parking",
+        navbar: {
+            drivers: "Conductores",
+            owners: "Propietarios",
+            about_us: "Nuestro Equipo",
+            about_product: "Nuestro Producto",
+        },
+        footer: {
+            copyright: "Todos los derechos reservados",
+            terms: "Términos y condiciones",
+            social_networks: "Nuestras Redes sociales",
+        },
+        home: {
+            first: "Tu espacio, tu oportunidad, tu viaje sin estrés,",
+            second: "Conduce directo a tu lugar reservado, o genera ingresos con tu cochera libre.",
+            third: "Bienvenido a la nueva era del estacionamiento",
+            call_to_action: "Descarga ya",
+            driver: "Soy conductor",
+            owner: "Soy propietario",
+            see_more: "Ver más",
+            know_more: "Conoce más",
+        },
+        driver: {
+            main: {
+                title: "Encuentra tu parking",
+                subtitle: "más cercano al mejor precio",
+                call_to_action: "Descarga Ya",
+            },
+            first: {
+                title: "Evita conducir por horas",
+                subtitle: "y no poder encontrar un lugar para estacionarte",
+                content: "Encontramos los parkings más cercanos a tu ubicación, con espacios libres para tí",
+            },
+            second: {
+                title: "Ahorra tiempo y gasolina",
+                subtitle: "y no vuelvas a perderlos buscando parkings",
+                content: "Encuentra los estacionamientos más cercanos en lugares que no conoces",
+            },
+            third: {
+                title: "Y no olvidemos el dinero",
+                subtitle: "Encuentra el estacionamiento más económico",
+                content: "Filtra los estacionamientos por el precio que cobren y encuentra el más barato cerca de tí"
+            },
+            plans: {
+                title: "Planes",
+                free: {
+                    title: "Free",
+                    features: [
+                        {
+                            text: "Solo puedes reservar un espacio al día",
+                        },
+                        {
+                            text: "Reserva con hasta 1 hora de anticipación",
+                        },
+                        {
+                            text: "Reserva espacios por horas",
+                        }
+                    ],
+                    money: "Gratis",
+                    price: "",
+                    frequency: "",
+                },
+                premium: {
+                    title: "Premium",
+                    features: [
+                        {
+                            text: "Hasta 5 reservas al día",
+                        },
+                        {
+                            text: "Reserva con hasta 24 horas de anticipación",
+                        },
+                        {
+                            text: "Reservar espacios por horas, días, o meses"
+                        }
+                    ],
+                    money: "S/.",
+                    price: "9.90",
+                    frequency: "al mes",
+                }
+            }
+        },
+        parking: {
+            main: {
+                title: "Registra tu estacionamiento",
+                subtitle: "y comienza a ganar dinero desde tu casa",
+                call_to_action: "Empieza ahora",
+            },
+            first: {
+                title: "Descubre al instante",
+                subtitle: "cuantos espacios libres tiene tu parking",
+                content: "Todo con un solo clic, al instante y sin tener que moverte de tu silla. Optimiza tu espacio y tiempo",
+            },
+            second: {
+                title: "Calcula el tiempo exacto",
+                subtitle: "que un vehículo estuvo en tu estacionamiento",
+                content: "Obtén el tiempo exacto que los vehículos estuvieron estacionados en tu parking",
+            },
+            third: {
+                title: "Da a conocer tu parking",
+                subtitle: "sin hacer una gran inversión, y de forma dinámica",
+                content: "Recomendamos tu estacionamiento a aquellos conductores que se encuentren cerca de él"
+            },
+            plans: {
+                title: "Planes",
+                free: {
+                    title: "Free",
+                    features: [
+                        {
+                            text: "Solo un parking registrado",
+                        },
+                        {
+                            text: "20 Parking spots como máximo",
+                        },
+                        {
+                            text: "Orden en búsqueda por defecto",
+                        }
+                    ],
+                    money: "Gratis",
+                    price: "",
+                    frequency: "",
+                },
+                premium: {
+                    title: "Premium",
+                    features: [
+                        {
+                            text: "Hasta 5 parkings registrados",
+                        },
+                        {
+                            text: "Hasta 100 parking spots",
+                        },
+                        {
+                            text: "Prioridad en búsquedas de usuarios",
+                        }
+                    ],
+                    money: "S/.",
+                    price: "50.00",
+                    frequency: "al mes",
+                }
+            }
+        },
+        about_us: {
+            title: "Nuestro Equipo",
+            content: "Somos un equipo de Padalustros, comprometidos con el desarrollo de la " +
+                "civilización de Age Mid Civilizations IV. Nos dedicamos a la creación de " +
+                "herramientas que faciliten la vida. En este caso, hemos creado una herramienta " +
+                "que permite a los conductores encontrar estacionamientos de forma rápida y " +
+                "eficiente, y a los propietarios de estacionamientos gestionar su espacio de " +
+                "forma rápida y eficiente. Nos apasiona la tecnología y la innovación, y estamos " +
+                "siempre buscando nuevas formas de mejorar la vida de las personas. Creemos que " +
+                "la tecnología puede ser una fuerza para el bien, y estamos comprometidos a " +
+                "utilizarla para hacer del mundo un lugar mejor. Nos encanta lo que hacemos, y " +
+                "estamos siempre buscando nuevas formas de mejorar nuestros productos y servicios.",
+            participants: [
+                {
+                    name: "Cortés Casas Joaquín Marcelo",
+                    code: "202114545",
+                    image: "3.jpg"
+                },
+                {
+                    name: "Diaz Silva Fernando Josué",
+                    code: "202112722",
+                    image: "4.jpg"
+                },
+
+                {
+                    name: "Arroyo Ormeño André Alonso",
+                    code: "202114714",
+                    image: "1.jpg"
+                },
+                {
+                    name: "Tafur Gonzales, Josty Gerardo",
+                    code: "20201c069",
+                    image: "6.jpg"
+                },
+                {
+                    name: "Zarate Caceres, Victor Ernesto",
+                    code: "202112907",
+                    image: "7.jpg"
+                },
+
+                {
+                    name: "Castilla Pachas, César Antonio",
+                    code: "202218735",
+                    image: "2.jpg"
+                },
+                {
+                    name: "Godoy, Johan Príncipe",
+                    code: "202014511",
+                    image: "5.jpg"
+                },
+            ],
+            video: "Video sobre el Equipo",
+            video_error: "Tu navegador no soporta la etiqueta de videos"
+        },
+        about_product: {
+            title: "Smart Parking",
+            description: "SmartParking es una solución innovadora diseñada para abordar los problemas " +
+                "asociados a la gestión ineficiente de estacionamientos en espacios públicos de alto " +
+                "tráfico.",
+            features: [
+                {
+                    text: "En muchas ciudades, encontrar estacionamiento toma tiempo y causa congestión, contaminación y malestar en los conductores.",
+                    icon: "pi pi-info-circle",
+                    upper: "Ver más",
+                },
+                {
+                    text: "SmartParking usa tecnología IoT para detectar espacios libres en tiempo real y mejorar la gestión de estacionamientos.",
+                    icon: "pi pi-warehouse",
+                    upper: "Ver más",
+                },
+                {
+                    text: "La app permite reservar espacios y pagar automáticamente, ofreciendo una experiencia cómoda y eficiente para todos.",
+                    icon: "pi pi-car",
+                    upper: "Ver más",
+                }
+            ],
+            video: "Video sobre el Producto",
+            video_error: "Tu navegador no soporta la etiqueta de videos"
+        }
+    },
+    EN: {
+        name: "Smart Parking",
+        navbar: {
+            drivers: "Drivers",
+            owners: "Owners",
+            about_us: "About the Team",
+            about_product: "About the Product",
+        },
+        footer: {
+            copyright: "All rights reserved",
+            terms: "Terms and conditions",
+            social_networks: "Our Social networks",
+        },
+        home: {
+            first: "Your space, your opportunity, your stress-free journey,",
+            second: "Drive straight to your reserved spot, or earn income with your free parking space.",
+            third: "Welcome to the new era of parking",
+            call_to_action: "Download now",
+            driver: "I’m a driver",
+            owner: "I’m an owner",
+            see_more: "See more",
+            know_more: "Know more",
+        },
+        driver: {
+            main: {
+                title: "Find your parking",
+                subtitle: "closest at the best price",
+                call_to_action: "Download now",
+            },
+            first: {
+                title: "Avoid driving for hours",
+                subtitle: "and not being able to find a place to park",
+                content: "We found the closest parkings to your location, with free spaces for you",
+            },
+            second: {
+                title: "Save time and gas",
+                subtitle: "and never lose them looking for parkings",
+                content: "Find the nearest parking lots to your location in places you don't know",
+            },
+            third: {
+                title: "Money is important too",
+                subtitle: "Find the cheapest parking lot per hour",
+                content: "Filter the parking lots near to your location according to the price they charge"
+            },
+            plans: {
+                title: "Plans",
+                free: {
+                    title: "Free",
+                    features: [
+                        {
+                            text: "You can only book one space per day",
+                        },
+                        {
+                            text: "Book up to 1 hour in advance",
+                        },
+                        {
+                            text: "Book spaces by the hour",
+                        }
+                    ],
+                    money: "Free",
+                    price: "",
+                    frequency: "",
+                },
+                premium: {
+                    title: "Premium",
+                    features: [
+                        {
+                            text: "Up to 5 bookings per day",
+                        },
+                        {
+                            text: "Book up to 24 hours in advance",
+                        },
+                        {
+                            text: "Book spaces by the hour, day, or month"
+                        }
+                    ],
+                    money: "S/.",
+                    price: "9.90",
+                    frequency: "per month",
+                }
+            }
+        },
+        parking: {
+            main: {
+                title: "Organize your space",
+                subtitle: "quickly and efficiently",
+                call_to_action: "Start now",
+            },
+            first: {
+                title: "Discover instantly",
+                subtitle: "how many free spaces your parking has",
+                content: "All with a single click, instantly and without having to move from your chair. Optimize your space and time",
+            },
+            second: {
+                title: "Calculate the exact time",
+                subtitle: "that a vehicle was in your parking lot",
+                content: "Get the exact time that vehicles were parked in your parking lot, so you can charge them",
+            },
+            third: {
+                title: "Your parking'll be known",
+                subtitle: "without making a big investment, and dynamically",
+                content: "We recommend your parking to those drivers who are near, and we do it for you"
+            },
+            plans: {
+                title: "Plans",
+                free: {
+                    title: "Free",
+                    features: [
+                        {
+                            text: "Only one registered parking",
+                        },
+                        {
+                            text: "Up to 20 parking spots",
+                        },
+                        {
+                            text: "Default search order",
+                        }
+                    ],
+                    money: "Free",
+                    price: "",
+                    frequency: "",
+                },
+                premium: {
+                    title: "Premium",
+                    features: [
+                        {
+                            text: "Up to 5 registered parkings",
+                        },
+                        {
+                            text: "Up to 100 parking spots",
+                        },
+                        {
+                            text: "Priority in user searches",
+                        }
+                    ],
+                    money: "S/.",
+                    price: "50.00",
+                    frequency: "per month",
+                }
+            }
+        },
+        about_us: {
+            title: "Our Team",
+            content: "We are a team of Padalustros, committed to the development of the " +
+                "civilization of Age Mid Civilizations IV. We are dedicated to creating " +
+                "tools that make life easier. In this case, we have created a tool that " +
+                "allows drivers to find parking lots quickly and efficiently, and parking " +
+                "owners to manage their space quickly and efficiently. We are passionate " +
+                "about technology and innovation, and we are always looking for new ways to " +
+                "improve people's lives. We believe that technology can be a force for good, " +
+                "and we are committed to using it to make the world a better place. We love " +
+                "what we do, and we are always looking for new ways to improve our products and services.",
+            participants: [
+                {
+                    name: "Cortés Casas Joaquín Marcelo",
+                    code: "202114545",
+                    image: "3.jpg"
+                },
+                {
+                    name: "Diaz Silva Fernando Josué",
+                    code: "202112722",
+                    image: "4.jpg"
+                },
+
+                {
+                    name: "Arroyo Ormeño André Alonso",
+                    code: "202114714",
+                    image: "1.jpg"
+                },
+                {
+                    name: "Tafur Gonzales, Josty Gerardo",
+                    code: "20201c069",
+                    image: "6.jpg"
+                },
+                {
+                    name: "Zarate Caceres, Victor Ernesto",
+                    code: "202112907",
+                    image: "7.jpg"
+                },
+
+                {
+                    name: "Castilla Pachas, César Antonio",
+                    code: "202218735",
+                    image: "2.jpg"
+                },
+                {
+                    name: "Godoy, Johan Príncipe",
+                    code: "202014511",
+                    image: "5.jpg"
+                },
+            ],
+            video: "Video about the Team",
+            video_error: "Your browser does not support the video tag"
+        },
+        about_product: {
+            title: "Smart Parking",
+            description: "SmartParking is an innovative solution designed to address the problems associated " +
+                "with inefficient parking management in high-traffic public spaces.",
+            features: [
+                {
+                    text: "In many cities, finding parking takes time and causes congestion, pollution, and discomfort for drivers.",
+                    icon: "pi pi-info-circle",
+                    upper: "See more",
+                },
+                {
+                    text: "SmartParking uses IoT technology to detect free spaces in real-time and improve parking management.",
+                    icon: "pi pi-warehouse",
+                    upper: "See more",
+                },
+                {
+                    text: "The app allows you to reserve spaces and pay automatically, offering a comfortable and efficient experience for everyone.",
+                    icon: "pi pi-car",
+                    upper: "See more",
+                }
+            ],
+            video: "Video about the Product",
+            video_error: "Your browser does not support the video tag"
+        }
+    }
+}
+export default in18
